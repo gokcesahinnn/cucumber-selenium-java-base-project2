bfa8f0e11bd395c4b7b01ddd0d6975e36b74311b
gsahin <gokce.sahin@kloia.com>
2023-01-27 10:10:38 +0300

    Report Eklendı

---
+++ src/main/java/com/javaBaseProject/web/qa/runners/SmokeTestRunner.java
@@ -0,0 +1,31 @@
+package com.javaBaseProject.web.qa.runners;
+
+
+import com.javaBaseProject.web.qa.util.ConfigReader;
+import com.vimalselvam.cucumber.listener.Reporter;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/com/javaBaseProject/web/qa/features"},
+        glue = "com.javaBaseProject.web.qa.steps",
+        plugin = "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportRegression.html",
+        tags = {"@smoke"}
+
+)
+
+public class SmokeTestRunner extends AbstractTestNGCucumberTests {
+    @AfterClass
+    public static void afterClass() {
+        Reporter.loadXMLConfig("src/main/resources/extendReportProperties.xml");
+        Reporter.setSystemInfo("QA Tester : ", "kloia QA");
+        Reporter.setSystemInfo("Application name : ", ConfigReader.get("application_name"));
+        Reporter.setSystemInfo("Operating System Info", ConfigReader.get("os_name"));
+        Reporter.setSystemInfo("Department", "QA");
+        Reporter.setTestRunnerOutput("Test execution Cucumber Report");
+    }
+}
